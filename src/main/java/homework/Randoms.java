cdabe2bda89ec1c4004875b82f65ca8b7edcd187
Nikita Novikov <monstars51@gmail.com>
2023-05-18 15:30:53 +0300

    all good

--- src/main/java/homework/Randoms.java
+++ src/main/java/homework/Randoms.java
@@ -5,18 +5,28 @@ import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
-//    protected final int MIN;
+    protected final int min;
+    protected final int max;
 
 
     public Randoms(int min, int max) {
+        this.min = min;
+        this.max = ++max;
         this.random = new Random();
-        //...
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
-    }
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
 
-    //...
+            @Override
+            public Integer next() {
+                return random.nextInt(min, max);
+            }
+        };
+    }
 }
